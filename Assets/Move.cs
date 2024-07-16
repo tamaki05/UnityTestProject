5ed62e402195616aa24188547ac80f9930221401
tamaki05 <kx0c0xk@gmail.com>
2024-07-16 23:52:17 +0900

    変数にする

--- Assets/Move.cs
+++ Assets/Move.cs
@@ -4,15 +4,21 @@ using UnityEngine;
 
 public class Move : MonoBehaviour
 {
+    // public‚ÅéŒ¾‚µ‚½•Ï”‚ÍInspector‚ÅŒã‚©‚ç•ÏX‚Å‚«‚é
+    public float speed = 1.5f;
+
     // Start is called before the first frame update
     void Start()
     {
+        // 1‰ñ‚¾‚¯Às
         Debug.Log("test");
     }
 
     // Update is called once per frame
     void Update()
     {
-        transform.Translate(Vector3.forward * 0.1f);
+        // ŒJ‚è•Ô‚µÀs
+        transform.Translate(Vector3.forward * speed);
+        Debug.Log(speed);
     }
 }
